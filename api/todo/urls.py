b92213ba4bdf3f8c2021b6a3d765e83a5794580f
adisak <sakengi@gmail.com>
2018-01-12 00:01:19 +0700

    add web

--- api/todo/urls.py
+++ api/todo/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+
 from todo import views
 
 urlpatterns = [
-    url(r'^todos/$', views.todo_list),
-    url(r'^todos/(?P<pk>[0-9]+)/$', views.todo_detail),
+    url(r'^todos/$', views.todo_list,name='todolist'),
+    url(r'^todos/(?P<pk>[0-9]+)/$', views.todo_detail,name='tododetail'),
+    url(r'^web/$', views.web_list ,name='weblist'),
+    url(r'^web/new/$', views.web_new ,name='webnew'),
+    url(r'^web/(?P<pk>[0-9]+)/$', views.web_detail ,name='webdetail'),
+
 ]
--- api/todo/views.py
+++ api/todo/views.py
@@ -1,15 +1,15 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.template import loader
+from django.http import HttpResponse
+
 from todo.models import Todo
 from todo.serializers import TodoSerializer
 
 
 @api_view(['GET', 'POST'])
 def todo_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
     if request.method == 'GET':
         todos = Todo.objects.all()
         serializer = TodoSerializer(todos, many=True)
@@ -24,9 +24,6 @@ def todo_list(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def todo_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
     try:
         todo = Todo.objects.get(pk=pk)
     except Snippet.DoesNotExist:
@@ -46,3 +43,35 @@ def todo_detail(request, pk):
     elif request.method == 'DELETE':
         todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def web_list(request):
+    if request.method == 'GET':
+        todo_list = Todo.objects.all()
+        template = loader.get_template('todo/index.html')
+        context = {
+            'todo_list': todo_list,
+        }
+        return HttpResponse(template.render(context, request))
+
+@api_view(['GET'])
+def web_new(request):
+    if request.method == 'GET':
+        template = loader.get_template('todo/new.html')
+        context = {
+        }
+        return HttpResponse(template.render(context, request))
+
+@api_view(['GET'])
+def web_detail(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        template = loader.get_template('todo/detail.html')
+        context = {
+            'todo': todo,
+        }
+        return HttpResponse(template.render(context, request))
